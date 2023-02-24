0e22ab351195268f369acabff896d5a6e61e182d
james <jamesrogersgis@gmail.com>
2023-02-23 20:52:21 -0600

    Stonks!

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+def example():
+    index = input("enter a the index you want to view: ")
+    data = ["Lion", "Sedan", "Cars", 110.00, 5.50, 3.30, 119.80, "2022-12-31"]
+    print(data[int(index)])
 
-def hello():
-    name = input('Hi what\'s your name?')
-    print('g\'day', name + '!')
-    age = input()
 
-
-hello()
+example()
