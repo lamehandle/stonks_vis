d6b6ec1b4e0a0fd71334455bf0c38974bbed6e86
lamehandle <jamesrogersgis@gmail.com>
2023-04-16 22:26:30 -0500

    WIP OOP refactor + plotly

--- plot.py
+++ plot.py
@@ -1,31 +1,23 @@
-import pandas as pd
-import plotly.graph_objects as go
-import plotly.offline as pyo
+import plotly.graph_objects as go  # MOre complex usage .express utilizes this module under the hood
+import plotly.offline as pyo  # allows for offline usage
+import stock as st  # Wrapper for yfinance
 import yfinance as yf
-import stock as st
-# import mplfinance as mpf
-pyo.init_notebook_mode()
+# pyo.init_notebook_mode()
 
-period = "1mo"
-# Valid periods are: 1d, 5d, 1mo, 3mo, 6mo, 1y, 2y, 5y, 10y, ytd, max:
+period = "1mo"  # Valid periods are: 1d, 5d, 1mo, 3mo, 6mo, 1y, 2y, 5y, 10y, ytd, max:
 
-dow = st.stock("^DJI")
-dow_hist = dow.data.history(period)
+# dow_raw.history(period)
 
-# Plot with additional subplot:
-mpf.plot(dow, volume=True, type="candle", addplot=subplot, style="yahoo")
-
-# df = yf.Ticker('AMZN').history(period='1mo')
-fig = go.Figure(
-    data=[
-        go.Candlestick(
-            x=df.index,
-            open=df['Open'],
-            high=df['High'],
-            low=df['Low'],
-            close=df['Close']
-        ),
-    ]
-)
-
+dow_raw = st.stock("^DJI")
+dow = yf.Ticker("^DJI").history(period)  # This data works todo refactor to use reset index properly
+print(dow)
+dow2 = dow.reset_index()  # This works because reset_index returns a new dataframe!!!!!
+print(dow2)
+# # Plot a candle stick plot with plotly
+fig = go.Figure(data=[go.Candlestick(x=dow2['Date'],
+                                     open=dow2['Open'],
+                                     high=dow2['High'],
+                                     low=dow2['Low'],
+                                     close=dow2['Close'],
+                                     )])
 fig.show()
